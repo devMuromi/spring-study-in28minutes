cd2650611633e579470412483fe5d210ba1b8375
devMuromi <devmuromi@gmail.com>
2024-03-25 13:18:23 +0900

    2.4: 강한 결합의 간단한 자바 코드 작성

---
+++ learn-spring-framework/src/main/java/com/devmuromi/learnspringframework/AppGamingBasicJava.java
@@ -0,0 +1,9 @@
+package com.devmuromi.learnspringframework;
+
+public class AppGamingBasicJava {
+    public static void main(String[] args) {
+        var marioGame = new MarioGame();
+        var gameRunner = new GameRunner(marioGame);
+        gameRunner.run();
+    }
+}
