49d1380bfb758a91e6acbb7c6a92a86dbed1406f
devMuromi <devmuromi@gmail.com>
2024-03-25 14:51:35 +0900

    2.10: Bean 추가 활용

--- learn-spring-framework/src/main/java/com/devmuromi/learnspringframework/App02HelloWorldSpring.java
+++ learn-spring-framework/src/main/java/com/devmuromi/learnspringframework/App02HelloWorldSpring.java
@@ -6,10 +6,14 @@ public class App02HelloWorldSpring {
     public static void main(String[] args) {
     var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
 
-    var str = context.getBean("name");
-    System.out.println(str);
+    System.out.println(context.getBean("name"));
     System.out.println(context.getBean("age"));
+
     System.out.println(context.getBean("person"));
-    System.out.println(context.getBean("address"));
+    System.out.println(context.getBean("person2MethodCall"));
+    System.out.println(context.getBean("person3Parameters"));
+
+    System.out.println(context.getBean("address2"));
+//    System.out.println(context.getBean(Address.class));
     }
 }
--- learn-spring-framework/src/main/java/com/devmuromi/learnspringframework/HelloWorldConfiguration.java
+++ learn-spring-framework/src/main/java/com/devmuromi/learnspringframework/HelloWorldConfiguration.java
@@ -3,7 +3,7 @@ package com.devmuromi.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-record Person(String name, int age) {}
+record Person(String name, int age, Address address) {}
 
 record Address(String firstLine, String city) {}
 
@@ -22,12 +22,27 @@ public class HelloWorldConfiguration {
 
     @Bean
     public Person person() {
-        var person = new Person("Rikka", 20);
+        var person = new Person("Rikka", 20, new Address("Pacific Ocean", "Virgo Cluster"));
         return person;
     }
 
     @Bean
+    public Person person2MethodCall() {
+        return new Person(name(), age(), address());
+    }
+
+    @Bean
+    public Person person3Parameters(String name, int age, Address address3) {
+        return new Person(name, age, address3);
+    }
+
+    @Bean(name = "address2")
     public Address address() {
         return new Address("Milky way", "Virgo Cluster");
+    }
+
+    @Bean
+    public Address address3() {
+        return new Address("Milky way3", "Virgo Super Cluster");
     }
 }
