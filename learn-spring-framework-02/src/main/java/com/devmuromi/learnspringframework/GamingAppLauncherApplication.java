13b9e8032d7ee4a70233fcbfb8fb3fd226d93ec0
devMuromi <devmuromi@gmail.com>
2024-03-26 10:14:55 +0900

    3.24: Spring에 Bean생성 위임하기

--- learn-spring-framework-02/src/main/java/com/devmuromi/learnspringframework/GamingAppLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/devmuromi/learnspringframework/GamingAppLauncherApplication.java
@@ -2,13 +2,29 @@ package com.devmuromi.learnspringframework;
 
 import com.devmuromi.learnspringframework.game.GameRunner;
 import com.devmuromi.learnspringframework.game.GamingConsole;
-import com.devmuromi.learnspringframework.game.PackmanGame;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-public class App03GamingStringBeans {
+@Configuration
+@ComponentScan("com.devmuromi.learnspringframework.game")
+public class GamingAppLauncherApplication {
+
+//    @Bean
+//    public PackmanGame packmanGame() {
+//        return new PackmanGame();
+//    }
+
+//    @Bean
+//    public GameRunner gameRunner(GamingConsole game) {
+//        System.out.println(game);
+//        var gameRunner = new GameRunner(game);
+//        return gameRunner;
+//    }
+
     public static void main(String[] args) {
         try (
-                var context = new AnnotationConfigApplicationContext(GamingConfiguration.class)
+                var context = new AnnotationConfigApplicationContext(GamingAppLauncherApplication.class)
         ) {
             context.getBean(GamingConsole.class).up();
             context.getBean(GameRunner.class).run();
