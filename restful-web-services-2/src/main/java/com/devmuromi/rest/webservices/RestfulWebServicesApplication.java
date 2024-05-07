779000ea333e8c68c3c72340a2c0697cebc7bc12
devMuromi <devmuromi@gmail.com>
2024-05-07 15:19:42 +0900

    12.230: TodoController 추가

--- restful-web-services-2/src/main/java/com/devmuromi/rest/webservices/RestfulWebServicesApplication.java
+++ restful-web-services-2/src/main/java/com/devmuromi/rest/webservices/RestfulWebServicesApplication.java
@@ -2,6 +2,9 @@ package com.devmuromi.rest.webservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
@@ -10,4 +13,12 @@ public class RestfulWebServicesApplication {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("*").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 }
